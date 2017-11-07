8ff294a2ad2a9a9d7db587e755113612420e36a7
ryanburns31892 <rburns@chapman.edu>
2017-11-06 18:32:41 -0800

    finished setup up plain-textview recyclerview & adapter

--- PickyLandlord/app/src/main/java/edu/chapman/cpsc356/pickylandlord/adapters/CrimeListAdapter.java
+++ PickyLandlord/app/src/main/java/edu/chapman/cpsc356/pickylandlord/adapters/CrimeListAdapter.java
@@ -1,26 +1,64 @@
 package edu.chapman.cpsc356.pickylandlord.adapters;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import edu.chapman.cpsc356.pickylandlord.CrimeCollection;
+import edu.chapman.cpsc356.pickylandlord.models.CrimeModel;
 
 
-public class CrimeListAdapter extends RecyclerView.Adapter
+public class CrimeListAdapter extends RecyclerView.Adapter<CrimeListAdapter.CrimeViewHolder>
 {
+    private final String LOGTAG = "CrimeListAdapter";
+
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
+    public CrimeViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
     {
-        return null;
+        Log.d(LOGTAG, "onCreateViewHolder()");
+
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View v = inflater.inflate(android.R.layout.simple_list_item_1, parent, false); // TODO: replace w/ custom
+
+        return new CrimeViewHolder(v);
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position)
+    public void onBindViewHolder(CrimeViewHolder holder, int position)
     {
+        Log.d(LOGTAG, "onBindViewHolder(" + position + ")");
+        // Get crime at specified position
+        CrimeModel crime = CrimeCollection.GetInstance().getCrimes().get(position);
 
+        // Setup the ViewHolder
+        holder.setup(crime);
     }
 
     @Override
     public int getItemCount()
     {
-        return 0;
+        Log.d(LOGTAG, "getItemCount()");
+        return CrimeCollection.GetInstance().getCrimes().size();
+    }
+
+    class CrimeViewHolder extends RecyclerView.ViewHolder
+    {
+        private TextView titleTextView;
+
+        public CrimeViewHolder(View itemView)
+        {
+            super(itemView);
+
+            this.titleTextView = (TextView) itemView;
+        }
+
+        public void setup(CrimeModel crime)
+        {
+            this.titleTextView.setText(crime.getTitle());
+        }
     }
 }
--- PickyLandlord/app/src/main/java/edu/chapman/cpsc356/pickylandlord/fragments/CrimeListFragment.java
+++ PickyLandlord/app/src/main/java/edu/chapman/cpsc356/pickylandlord/fragments/CrimeListFragment.java
@@ -3,6 +3,7 @@ package edu.chapman.cpsc356.pickylandlord.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +14,7 @@ import java.util.List;
 
 import edu.chapman.cpsc356.pickylandlord.CrimeCollection;
 import edu.chapman.cpsc356.pickylandlord.R;
+import edu.chapman.cpsc356.pickylandlord.adapters.CrimeListAdapter;
 import edu.chapman.cpsc356.pickylandlord.models.CrimeModel;
 
 
@@ -25,7 +27,10 @@ public class CrimeListFragment extends Fragment
         View v = inflater.inflate(R.layout.fragment_crime_list, container, false);
 
         RecyclerView crimesListView = v.findViewById(R.id.rv_crimes);
-        // crimesListView.setAdapter();
+        crimesListView.setAdapter(new CrimeListAdapter());
+
+        // This is lame - don't study this, just do it
+        crimesListView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
         return v;
     }
